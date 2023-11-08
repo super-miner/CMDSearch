43c14478be874a637daa88c875a16cfde8b63049
Flown <105561081+super-miner@users.noreply.github.com>
2023-11-08 11:52:49 -0800

    Commit before rework

--- CMDSearch/CMDCallback.cs
+++ CMDSearch/CMDCallback.cs
@@ -2,8 +2,14 @@
 
 namespace CMDSearch; 
 
-public struct CMDCallback {
+public class CMDCallback {
     public int MatchesFound;
     public Regex Trigger;
     public Action<Match> Callback;
+
+    public CMDCallback(int matchesFound, Regex trigger, Action<Match> callback) {
+        MatchesFound = matchesFound;
+        Trigger = trigger;
+        Callback = callback;
+    }
 }
--- CMDSearch/CMDObject.cs
+++ CMDSearch/CMDObject.cs
@@ -5,6 +5,8 @@ namespace CMDSearch;
 
 public class CMDObject {
     private string _batchFilePath;
+    private bool _logProcessOutput;
+    
     private Process _process;
     private StreamReader _processReader;
     private StreamWriter _processWriter;
@@ -13,12 +15,16 @@ public class CMDObject {
     private Mutex _readerMutex = new Mutex();
     private List<CMDCallback> _callbacks = new List<CMDCallback>();
 
-    public CMDObject(string batchFilePath, string[] args, bool createWindow = false) {
+    private Mutex _threadStateMutex = new Mutex();
+    private bool closeThread = false;
+
+    public CMDObject(string batchFilePath, string[] args, bool logProcessOutput = false, bool showWindow = false) {
+        _logProcessOutput = logProcessOutput;
         _batchFilePath = batchFilePath;
 
         string command = $"\"{batchFilePath}\" {string.Join(' ', args)}";
         ProcessStartInfo processInfo = new ProcessStartInfo("cmd.exe", $"/c {command}");
-        processInfo.CreateNoWindow = !createWindow;
+        processInfo.CreateNoWindow = !showWindow;
         processInfo.UseShellExecute = false;
         processInfo.RedirectStandardInput = true;
         processInfo.RedirectStandardOutput = true;
@@ -26,6 +32,7 @@ public class CMDObject {
         _process = Process.Start(processInfo) ?? throw new NullReferenceException();
         
         _process.Exited += OnProcessExited;
+        _process.Disposed += OnProcessExited;
 
         _processReader = _process.StandardOutput;
         _processWriter = _process.StandardInput;
@@ -40,11 +47,7 @@ public class CMDObject {
     public void AddCallback(Regex trigger, Action<Match> callback) {
         _readerMutex.WaitOne();
         
-        _callbacks.Add(new CMDCallback() {
-            Callback = callback,
-            MatchesFound = 0,
-            Trigger = trigger
-        });
+        _callbacks.Add(new CMDCallback(0, trigger, callback));
         
         _readerMutex.ReleaseMutex();
     }
@@ -54,35 +57,71 @@ public class CMDObject {
     }
     
     private void WatchStream() {
+        SynchronizationContext mainThreadContext = SynchronizationContext.Current;
+        
         _readerTask = Task.Run(() => {
             string accumulator = "";
             
             while (true) {
-                int characterCode = _processReader.Read();
-
-                if (characterCode == -1) {
-                    continue;
+                _threadStateMutex.WaitOne();
+                if (closeThread) {
+                    closeThread = false;
+                    
+                    break;
                 }
+                _threadStateMutex.ReleaseMutex();
+                
+                Debug.WriteLine("Debug 1");
 
-                char character = (char)characterCode;
+                bool accumulatorChanged = false;
+                while (true) {
+                    int characterCode = _processReader.Read();
 
-                accumulator += character;
+                    if (characterCode == -1) {
+                        Debug.WriteLine("Debug A");
+                        
+                        break;
+                    }
+                    
+                    Debug.WriteLine("Debug B, " + characterCode);
+
+                    char character = (char)characterCode;
+
+                    accumulator += character;
+                    accumulatorChanged = true;
+
+                    Debug.WriteLine("Debug 2");
+                }
+                
+                Debug.WriteLine("Debug 3");
+
+                if (_logProcessOutput && accumulatorChanged) {
+                    Debug.WriteLine("\n-------- PROCESS START --------");
+                    Debug.WriteLine(accumulator);
+                    Debug.WriteLine("------- PROCESS CURRENT -------\n");
+                }
 
                 _readerMutex.WaitOne();
 
                 foreach (CMDCallback callback in _callbacks) {
                     MatchCollection matches = callback.Trigger.Matches(accumulator);
-
+                    
                     if (matches.Count <= callback.MatchesFound) {
                         continue;
                     }
 
                     int matchesLengthDelta = matches.Count - callback.MatchesFound;
-                    MatchCollection matchesDelta = (MatchCollection) matches.Skip(Math.Max(0, matches.Count - matchesLengthDelta));
-
-                    foreach (Match match in matchesDelta) {
-                        callback.Callback.Invoke(match);
+                    IEnumerable<Match> matchesDelta = matches.Skip(Math.Max(0, matches.Count - matchesLengthDelta));
+                    
+                    foreach (Match match in matchesDelta) { 
+                        if (match.Success) {
+                            mainThreadContext.Post(_ => {
+                                callback.Callback.Invoke(match);
+                            }, null);
+                        }
                     }
+                    
+                    callback.MatchesFound = matches.Count;
                 }
                 
                 _readerMutex.ReleaseMutex();
@@ -91,7 +130,11 @@ public class CMDObject {
     }
 
     public void Destroy() {
-        _readerTask.Dispose();
+        _threadStateMutex.WaitOne();
+        
+        closeThread = true;
+        
+        _threadStateMutex.ReleaseMutex();
         
         _processWriter.Close();
         _processReader.Close();
@@ -99,6 +142,8 @@ public class CMDObject {
     }
     
     private void OnProcessExited(object? sender, EventArgs args) {
+        Debug.WriteLine("Process Exited");
+        
         Destroy();
     }
 }
